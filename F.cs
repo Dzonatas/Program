8f101ce9473429a79979919483f6c7965955a246
Dzonatas <dzonatas@gmail.com>
2013-08-22 11:08:25 -0700

    Project-Managers MUST upgrade to F# from spreadsheet.bat. (80/20)

--- F.cs
+++ F.cs
@@ -1,5 +1,6 @@
-using System;
-
 public partial class A335
-		{
-		}
+{
+//_FIX:"flags","flag",_FIXT:LOCATION.[mip]map,_var|enum,_ref:Icesphere.Panel.Map-polar(<F#,-.bat(D)>)
+//_FIXT:f_script(<s>javascript</s>+.bat)[,Torqu3D,LLVM][[_loops]]
+//_FIXT:_
+}
