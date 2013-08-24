c2814c9f9363935bdd9db63a9a0ee8f8b82162c4
Dzonatas <dzonatas@gmail.com>
2013-08-24 06:24:22 -0700

    Atomatrix synchronized accounts. (T`ent-ity)(@_)

--- E.cs
+++ E.cs
@@ -3,6 +3,11 @@ using System;
 public partial class A335
 {
 
+/*
+jo <ent,ity>{'&'<DOTTEDNAME/>';'}
+jo <ent,`>{-'&'_ _';'+}               //_arc[-hive]
+*/
+
 static void end()
 	{
 	B_end() ;
--- V.cs
+++ V.cs
@@ -1,5 +1,5 @@
 public partial class A335
 {
 // jo <volume,syllable>{.pro-<hydro-_><oxy-_/><oxy-_/></hydro-_>-gen}   //_FIXT:_no_center?_OK,_obvious
-// jo <volume,trans>{.pro-<hydro-_><oxy--/><oxy-+/></hydro-_>-gen}      //_FIX:proton,photon[,_antimatter]
+// jo <volume,trans>{.pro-<hydro-_><oxy-&minus;/><oxy-&plus;/></hydro-_>-gen}      //_FIX:proton,photon[,_antimatter]
 }
