1e250afc49777b85a1464a8569aaf8e165baa670
Dzonatas <dzonatas@gmail.com>
2013-08-24 10:54:35 -0700

    UNICODED

--- Z.cs
+++ Z.cs
@@ -1,5 +1,17 @@
-using System;
-
 public partial class A335
-		{
-		}
+{
+#region micro
+/*
+<_.../t....=>
+*/
+#endregion micro
+}
+
+/*
+commit 4b00444872ef9d4d1fcb396ae87a4f01b7dc4d81
+Author: Dzonatas <dzonatas@gmail.com>
+Date:   Sat Aug 24 10:41:21 2013 -0700
+
+    _JHB
+    Signed-off-by: Dzonatas <dzonatas@gmail.com>
+*/
