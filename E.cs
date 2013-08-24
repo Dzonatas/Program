fee2c88696c58ae870860f2c3683b169a57c4ece
Dzonatas <dzonatas@gmail.com>
2013-08-24 06:43:09 -0700

    Resolved bang-paths to entities. (Trigraphed)

--- E.cs
+++ E.cs
@@ -4,8 +4,10 @@ public partial class A335
 {
 
 /*
-jo <ent,ity>{'&'<DOTTEDNAME/>';'}
+jo <ent,ity>{'&'<DOTTEDNAME>';'}
 jo <ent,`>{-'&'_ _';'+}               //_arc[-hive]
+jo <ent,`.>{+'.'-}                    //_WS_OR_$$!
+jo <ent,`!>{'!'}                      //_FIXT:[_dirty_async_end]
 */
 
 static void end()
