6b56e9056b81ff81646684ab72500326c9fedb9d
Dzonatas <dzonatas@gmail.com>
2013-08-24 08:37:22 -0700

    Only XML can 'break' the kernel now. (Silenced)((XAML.Sat))

--- E.cs
+++ E.cs
@@ -1,10 +1,10 @@
-using System;
+//using System;
 
 public partial class A335
 {
 
 /*
-jo <ent,ity>{'&'<DOTTEDNAME>';'}
+jo <ent,ity>{'&'<DOTTEDNAME>';'}      //"$entity...
 jo <ent,`>{-'&'_ _';'+}               //_arc[-hive]
 jo <ent,`.>{+'.'-}                    //_WS_OR_$$!
 jo <ent,`!>{'!'}                      //_FIXT:[_dirty_async_end]
