ed5a2f2122c569a46df7ede87e20e68ad35ca4f6
Dzonatas <dzonatas@gmail.com>
2013-08-25 13:02:54 -0700

    [][](->[",\'])

--- H.cs
+++ H.cs
@@ -1,5 +1,18 @@
-using System;
-
 public partial class A335
-		{
-		}
+{
+
+/*
+atomic struct bo.jo : <T,t> [, [remote]]    //_if(T==$undefined) T=$anonymous,t=$global
+	{
+	[remote] var() { entity: return [ice] lock<T,t> {} }  //_FIXT:(RT)_gen_t,_no_ice_no_m
+	}
+	
+bo.jo
+ {
+ if( "P=NP" is [illogical] )     //_SOLUTION:given,_tm,_MUST_TYPE((.NET.or.&trademark;).TYPE.assert.same(~equality)),_[!]URN
+ 	return $this.method() ;
+ return $caller method() ; //_FIXT:<s>breaks_DOTTEDNAME</s>,_(P)!=(NP),_`',_FIX:_do_not_(taint)
+ }
+*/
+
+}
