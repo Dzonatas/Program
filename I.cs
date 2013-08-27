cf1d254ff1e73544da187f2f81376427128cc458
Dzonatas <dzonatas@gmail.com>
2013-08-26 19:24:04 -0700

    <ice></>

--- I.cs
+++ I.cs
@@ -13,6 +13,8 @@ public partial class A335
 //string input = "<?xml version=\"1.0\" ?>\n<xml reflection-i><a f=\"1\">b</a><c>d</c></xml>" ;
 //string input = "<?xml version=\"0.1\" ?>\n<xml reflection--i><a f=\"1\">b</a><c>d</c></xml>" ;
 //string input = "<?xml version=\"0.0\" ?>\n<xml reflection--i><a f=1>b</a><c>d</c></xml>" ;
+//string input = "N0P<?xml version=(0.0) ?>\n<xml reflection-ice-i><a f=1>b</a><c>d</c></xml>" ;
+//string input = "<xml reflection-ice-i><a f=1>b</a><c>d</c></xml>" ;
 
 public struct Itemset
 	{
