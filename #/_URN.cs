d7a8f7be67dce0be6c9d3d7a96b18cde40f3b2df
Dzonatas <dzonatas@gmail.com>
2013-08-29 11:28:12 -0700

    [.urn#_URN:""] Deleted _URN.cs ; (.move ./#/_URN.cs [to] ".urn.git" post.)

--- #/_URN.cs
+++
@@ -1,5 +0,0 @@
-//[assembly: AssemblyDescription("(Sound)")]
-[assembly: AssemblyVersion("0.0.0.1")]
-//.(in-order).{0}.{0}.{0}.{(0->1)}.{"restricted:","domain"(,[.,])}
-//.{["URN:",.]}
-//({["URN:",(-)]}).
