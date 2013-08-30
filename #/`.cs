a1f09f1e5595b2fa0b6ff33fe659563d86642cb5
Dzonatas <dzonatas@gmail.com>
2013-08-30 08:05:29 -0700

    _`'

--- #/`.cs
+++ #/`.cs
@@ -6,7 +6,7 @@ Date:   Sat Aug 24 10:41:21 2013 -0700
     _JHB
     Signed-off-by: Dzonatas <dzonatas@gmail.com>
 */
-//|#(!)using System;//.[,,,(One)](_).(_)(.o...)(___)(.a)(_s)
+//|#(!)using System;//y[,,,(One)](_).(_)(.o...)(___)(.a)(_s)
 
 public partial class _
 {
