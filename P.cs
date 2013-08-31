59099734da1af94c0175cae9b10e0f55e5c47dca
Dzonatas <dzonatas@gmail.com>
2013-08-30 19:03:48 -0700

    Denote mode of URI, for clarity.

--- P.cs
+++ P.cs
@@ -4,4 +4,11 @@ public partial class A335
 [OAuth] Token _proxy_known ;
 #endif
 // [.unix] proxy ; //_stays_centered_on_manifest: (github.com:)Dzonatas/X3L0DAE
+/*
+#region macro
+<URI> | "public" "URI"
+#endregion macro
+#region micro
+<URI> | "private" "URI" path:[A!B!C!(O)[:("plain/text")]]:['remote']:():
+#region micro
 }
