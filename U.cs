2356228520ee63db1e9e8de0acb2f3153cd1f1f3
Dzonatas <dzonatas@gmail.com>
2013-08-31 19:13:57 -0700

    Memory streams with offsets, de-BSD'd (HTTP).

--- U.cs
+++ U.cs
@@ -3,7 +3,12 @@ public partial class A335
 #if UNIX
 static UNIX_HTML_EBCDIC_ flag_IMAGE_x ;
 #endif
+/*
 #region micro
 //_page[book,.] | <URL>
+<URI>           | <URN>.<URL>
+<URI>           | ((Decimal).(Decimal))
+<URI>           | [N,L(.P)] 
 #endregion micro
+*/
 }
