b7294211e298978200a6eb09356ae2cf07bb71ff
Dzonatas <dzonatas@gmail.com>
2013-08-31 18:24:36 -0700

    (XML) Assimulate "JavaScript Object-Notation" for various flags.

--- F.cs
+++ F.cs
@@ -1,6 +1,6 @@
 public partial class A335
 {
-//_FIX:"flags","flag",_FIXT:LOCATION.[mip]map,_var|enum,_ref:Icesphere.Panel.Map-polar(<F#,-.bat(D)>)
+//_FIX[t]:<s>"flags"</>,<s>"flag"</>,_FIXT:LOCATION.[mip]map,_var|enum,_ref:Icesphere.Panel.Map-polar(<F#,-.bat(D)>)
 //_FIXT:f_script(<s>javascript</s>+.bat)[,Torqu3D,LLVM][[_loops]]
 //_FIXT:_(futh_)-((_second))
 
