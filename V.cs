804ae10b1614944c03ea06028763f9c20a8df5cb
Dzonatas <dzonatas@gmail.com>
2013-09-02 14:05:38 -0700

    (Descriptive) Voxel by "well-known" convexion, (voxel|HTTP).

--- V.cs
+++ V.cs
@@ -6,4 +6,9 @@ const int voxel_volume = 0.000 ; //_unknown_orientation
 // jo <volume,trans`>{.pro-<hydro-_><oxy-&minus-(cc.0);/><oxy-&plus-p;/></hydro-_>-gen}      //_FIX:proton,photon[,_antimatter-m.()_nt:]
 /* <s>protected</> */ static int no_p ; //[N0P]:'oprand.'(.)'//y' //_after:protected.flag.thrown
 //'URN:((dv(:)(d)).0xbf)'//y
+#region V
+#region VV
+//<VOXEL VOLUME="[0.0,0.0]"/>//
+#endregion VV
+#enregion V
 }
