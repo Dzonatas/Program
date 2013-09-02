077c472ed0f9eceef57f558dbc832b26234f5da0
Dzonatas <dzonatas@gmail.com>
2013-09-02 09:10:12 -0700

    (...comments]

--- F.cs
+++ F.cs
@@ -1,5 +1,6 @@
 public partial class A335
 {
+int f_rune_sharp ; //#'s (-> ./#/...)
 //_FIX[t]:<s>"flags"</>,<s>"flag"</>,_FIXT:LOCATION.[mip]map,_var|enum,_ref:Icesphere.Panel.Map-polar(<F#,-.bat(D)>)
 //_FIXT:f_script(<s>javascript</s>+.bat)[,Torqu3D,LLVM][[_loops]]
 //_FIXT:_(futh_)-((_cubed)(_second))
