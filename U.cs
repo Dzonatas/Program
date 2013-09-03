ac1694508d92c1eb65a6c829af1bd07a3464a6b7
Dzonatas <dzonatas@gmail.com>
2013-09-03 05:00:07 -0700

    (Relax XML) .ico files as flags if not defaulted by EBCDIC.

--- U.cs
+++ U.cs
@@ -1,7 +1,7 @@
 public partial class A335
 {
 #if UNIX
-static UNIX_HTML_EBCDIC_ flag_IMAGE_x ;
+static UNIX_HTML_EBCDIC_ flag_IMAGE_x ; //_FIX:HTML_EBCDIC->('flag'.ico)
 static UD P ; //| 'prototype' [static]
 #endif
 /*
