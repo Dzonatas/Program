255eda450166ba724cfcbe1d5ab1191cb08e4e2c
Dzonatas <dzonatas@gmail.com>
2013-09-03 07:48:33 -0700

    Cascaded cases use (flags), capitals overrode ("default:capitol".ico).

--- F.cs
+++ F.cs
@@ -3,6 +3,7 @@ public partial class A335
 int f_rune_sharp ; //#`s (-> ./#/...)
 //int f_rune__   ; //# (-> ./#/__.(./#/__...])//
 int f_rune___    ; //# (-> ./#/___]
+const int flag_rune__    ; //# (-> ./#/___.(./#/_]](.ico)
 //_FIX[t]:<s>"flags"</>,<s>"flag"</>,_FIXT:LOCATION.[mip]map,_var|enum,_ref:Icesphere.Panel.Map-polar(<F#,-.bat(D)>)
 //_FIXT:f_script(<s>javascript</s>+.bat)[,Torqu3D,LLVM][[_loops]]
 //_FIXT:_(futh_)-((_cubed)(_second))
