5617fdad26bb87c18a6fe51073acf42e2c2de0ae
Dzonatas <dzonatas@gmail.com>
2013-09-03 12:07:35 -0700

    (e.g.) Without the i-replicator, it's not artificial-work.

--- E.cs
+++ E.cs
@@ -3,7 +3,7 @@
 public partial class A335
 {
 string eg = "I.E." ; //_FIXT:"handle"[,_ice_no][,_voxel][,enterprise:LEVEL:FILE:(.md)]
-//URN:EARTH:
+//URN:EARTH:EARTHEARTHEARTH-...-.../\_.
 
 /*
 jo <ent,ity>{'&'<DOTTEDNAME>';'}      //"$entity...
