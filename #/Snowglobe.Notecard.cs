dd51ae7f93e6161427ca1a561050db2a30e6844b
Dzonatas <dzonatas@gmail.com>
2013-09-04 12:53:08 -0700

    UUID Index inventory, network-order auto-boxes, (firewalls).

---
+++ #/Snowglobe.Notecard.cs
@@ -0,0 +1,25 @@
+/* License: GNU Public License version 2, or any later version of the GNU Public License.
+**          http://www.gnu.org/licenses/gpl-2.0.html
+** Copyright (C) 2009 Ballard, Jonathan H. <dzonatas@dzonux.net> 
+*/
+/*
+using System ;
+
+namespace Snowglobe
+	{
+	public class Notecard
+		{
+		Inventory.Item   item ;
+
+		public UUID ID
+			{
+			get { return item.ID ; }
+			}
+		
+		public Notecard( UUID id )
+			{
+			item = Inventory.FindItem( id ) ;
+			}
+		}
+	}
+*/
