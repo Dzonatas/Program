04f042dc6fe2df919541e7e70165f70c0021aad3
Dzonatas <dzonatas@gmail.com>
2013-09-05 17:17:28 -0700

    Contributor structure, xs:URL:String,String,String,String.

--- C.cs
+++ C.cs
@@ -8,3 +8,18 @@ partial class A335
 */
 #endregion macro
 }
+
+namespace Contributors
+	{
+	struct Contributor
+		{
+		string node_asset_contribtor =
+		"<embed id              = \"xs:String\">
+			Author          = \"<THIS/>\"
+			Authoring_Tool  = \"<THIS/>\"
+			Comments        = \"<THIS/>\"
+			Copyright       = \"<THIS/>\"
+			Source_Data     =  "+URL+"
+		        </embed>";
+		}
+	}
