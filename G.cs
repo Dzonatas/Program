64c6d102a50de69f871369cf7a7ed6de6ad5c13e
Dzonatas <dzonatas@gmail.com>
2013-09-06 13:23:33 -0700

    (Shared-(opened)) A -> (GLASS:ANDROID:{}).JS:

--- G.cs
+++ G.cs
@@ -6,7 +6,11 @@ public partial class A335
 //'['.header:'URN':']'_g_xml(_env:[P]) ;//
 //'['.footer']'_g_xml(_env:[P]) ;//_NP[<s>"C++"</>,entry[....]]
 */
+#if GLASS
 //<svg:switch registry="g-"/>
 //<Geospatial id="object:voxel"/>
 //new (Gtk).REST.Panel.(Material) ;//_atomic_end('{".javaScript"</>:}')
+#elif A
+//<svg:switch/>//https://plus.google.com/100758507506787481234/posts/SswB18fhRC6
+#endif
 }
