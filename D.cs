8e631b45d4dbdf2cbac2310342b6335ad24fc1f9
Dzonatas <dzonatas@gmail.com>
2013-09-07 02:42:37 -0700

    [__default_device,_delta_shape] -> stateful_icon ; n.n.

--- D.cs
+++ D.cs
@@ -12,10 +12,21 @@ partial class A335
 public static Decimal Dzonexus ; //_dzonux.NET!(sendme:(vector|scalar|"sector"){(".get"|git:"voxel")})(-)
 static string dna_dex_nt ; //_foreach(/DNA/).active.idle.up //_FIX:_"inventory"_events,"WebSockets",'('iDNA')',_FIXT:(+)"IHAVE:"
 static bool debug ; //_passive((_d))_##IPVI
-string __default_device ; //_google_play,_XSLT_DO_NOT_TRACK(_replicated)
+//string __default_device ; //_google_play,_XSLT_DO_NOT_TRACK(_replicated)
 //string _d_io_delta_shape ;
 //_FIXT:_[active|inactive]_predicates[,_try_ActiveX,_new_Item()]
 //_FIXT:(bit.switch),INSTR_FOR...N0P[,_canonical][,_blitter]
 //_FIX:_stackless-C[,_hyper-|_math__][,_embedded]
 //static int d_linear_d    ; //_simulate:intranet[,_override_lineage_and_raise_hexed_flag]
 }
+
+namespace Directory
+	{
+	struct _resource
+		{
+		struct _ico
+			{
+			Decimal stateful_icon ; //_default:env-file:.ico
+			}
+		}
+	}
