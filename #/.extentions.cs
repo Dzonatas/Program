408aa2d921cb1377f725ffdc42862f2e43a4b522
Dzonatas <dzonatas@gmail.com>
2013-09-30 16:28:06 -0700

    Background channel defaults to movie content, static-screen-size-rate.

--- #/.extentions.cs
+++ #/.extentions.cs
@@ -240,5 +240,13 @@ namespace System.Extensions
 			extern static IntPtr initialize_threads() ;
 		#endif
 
+
+		class Channel   // : X-Window
+			{
+			//VisualType:StaticGray|StaticColor|GrayScale
+			//WINGRAVITY:Static
+			//BITGRAVITY:Static
+			//Depth:Array
+			}
 		}
 	}
