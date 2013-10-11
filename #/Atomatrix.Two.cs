dbc13dfdab92b7562103327df2e9ab162bf361ae
Dzonatas <dzonatas@gmail.com>
2013-10-11 08:32:15 -0700

    L-R Type system with S/MIME, synchronous-mime.

---
+++ #/Atomatrix.Two.cs
@@ -0,0 +1,59 @@
+namespace Atomatrice
+{
+using System ;
+static public class Two
+	{
+	static IntPtr _precursor  = (IntPtr)01010100010101 ;
+	static IntPtr _intraface  = (IntPtr)01010101010010 ;
+	static IntPtr _intrafaces = (IntPtr)01010101010101 ;
+	static IntPtr _orbit      = (IntPtr)01010101011010 ;
+	static IntPtr _interface  = (IntPtr)01011101010101 ;
+	static IntPtr _interfaces = (IntPtr)01010101010101 ;
+	static IntPtr _item       = (IntPtr)01010101010101 ;
+	static public IntPtr Precursor
+		{
+		get { return _precursor ; }
+		set { _precursor = value ; }
+		}
+	static public IntPtr Intraface
+		{
+		get { return _intraface ; }
+		set { _intraface = value ; }
+		}
+	static public IntPtr Intrafaces
+		{
+		get { return _intrafaces ; }
+		set { _intrafaces = value ; }
+		}
+	static public IntPtr Orbit
+		{
+		get { return _orbit ; }
+		set { _orbit = value ; }
+		}
+	static public IntPtr Interface
+		{
+		get { return _interface ; }
+		set { _interface = value ; }
+		}
+	static public IntPtr Interfaces
+		{
+		get { return _interfaces ; }
+		set { _interfaces = value ; }
+		}
+	static public IntPtr Mime
+		{
+		get { return _item ; }
+		set { _item = value ; }
+		}
+	static public IntPtr Type(this IntPtr a, int offset)
+		{
+		return IntPtr.Subtract(_item,offset) ;
+		} 
+	}
+	/*
+	.
+	.
+	.
+	*/
+	
+}
