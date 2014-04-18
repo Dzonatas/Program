a74254335c583ac579715c6b16c48204c1ed7b4b
Dzonatas <dzonatas@gmail.com>
2014-04-18 07:12:16 -0700

    The rest of those methods that compiles HelloWorld.asm.

--- J.cs
+++ J.cs
@@ -144,6 +144,61 @@ static private object[] assemblyRefDecls_assemblyRefDecls_assemblyRefDecl( objec
 	{
 	return o ;
 	}
+	
+static private object[] decl_assemblyRefHead_____assemblyRefDecls____( object[] o ) { return o ; }
+static private object[] decls_decls_decl( object[] o ) { return o ; }
+static private object[] id_SQSTRING( object[] o ) { return o ; }
+static private object[] assemblyHead___assembly__asmAttr_name1( object[] o ) { return o ; }
+static private object[] assemblyDecl___hash___algorithm__int32( object[] o ) { return o ; }
+static private object[] assemblyDecls_assemblyDecls_assemblyDecl( object[] o ) { return o ; }
+static private object[] assemblyDecl_asmOrRefDecl( object[] o ) { return o ; }
+static private object[] decl_assemblyHead_____assemblyDecls____( object[] o ) { return o ; }
+static private object[] classAttr_classAttr__private_( object[] o ) { return o ; }
+static private object[] classAttr_classAttr__auto_( object[] o ) { return o ; }
+static private object[] classAttr_classAttr__ansi_( object[] o ) { return o ; }
+static private object[] classAttr_classAttr__beforefieldinit_( object[] o ) { return o ; }
+static private object[] name1_DOTTEDNAME( object[] o ) { return o ; }
+static private object[] slashedName_name1( object[] o ) { return o ; }
+static private object[] className_____name1_____slashedName( object[] o ) { return o ; }
+static private object[] extendsClause__extends__className( object[] o ) { return o ; }
+static private object[] classHead___class__classAttr_id_extendsClause_implClause( object[] o ) { return o ; }
+static private object[] methodHeadPart1___method_( object[] o ) { return o ; }
+static private object[] methAttr_methAttr__public_( object[] o ) { return o ; }
+static private object[] methAttr_methAttr__hidebysig_( object[] o ) { return o ; }
+static private object[] methAttr_methAttr__specialname_( object[] o ) { return o ; }
+static private object[] methAttr_methAttr__rtspecialname_( object[] o ) { return o ; }
+static private object[] callKind__default_( object[] o ) { return o ; }
+static private object[] callConv_callKind( object[] o ) { return o ; }
+static private object[] callConv__instance__callConv( object[] o ) { return o ; }
+static private object[] type__void_( object[] o ) { return o ; }
+static private object[] methodName___ctor_( object[] o ) { return o ; }
+static private object[] implAttr_implAttr__cil_( object[] o ) { return o ; }
+static private object[] implAttr_implAttr__managed_( object[] o ) { return o ; }
+static private object[] methodHead_methodHeadPart1_methAttr_callConv_paramAttr_type_methodName_____sigArgs0_____implAttr____( object[] o ) { return o ; }
+static private object[] methodDecl___maxstack__int32( object[] o ) { return o ; }
+static private object[] methodDecls_methodDecls_methodDecl( object[] o ) { return o ; }
+static private object[] methodDecl_id____( object[] o ) { return o ; }
+static private object[] instr_INSTR_NONE( object[] o ) { return o ; }
+static private object[] methodDecl_instr( object[] o ) { return o ; }
+static private object[] type__valuetype__className( object[] o ) { return o ; }
+static private object[] typeSpec_type( object[] o ) { return o ; }
+static private object[] instr_INSTR_METHOD_callConv_type_typeSpec______methodName_____sigArgs0____( object[] o ) { return o ; }
+static private object[] classDecl_methodHead_methodDecls____( object[] o ) { return o ; }
+static private object[] classDecls_classDecls_classDecl( object[] o ) { return o ; }
+static private object[] methAttr_methAttr__static_( object[] o ) { return o ; }
+static private object[] methodName_name1( object[] o ) { return o ; }
+static private object[] methodDecl___entrypoint_( object[] o ) { return o ; }
+static private object[] compQstring_QSTRING( object[] o ) { return o ; }
+static private object[] instr_INSTR_STRING_compQstring( object[] o ) { return o ; }
+static private object[] type__class__className( object[] o ) { return o ; }
+static private object[] type__string_( object[] o ) { return o ; }
+static private object[] sigArg_paramAttr_type( object[] o ) { return o ; }
+static private object[] sigArgs1_sigArg( object[] o ) { return o ; }
+static private object[] sigArgs0_sigArgs1( object[] o ) { return o ; }
+static private object[] decl_classHead_____classDecls____( object[] o ) { return o ; }
+static private object[] START_decls( object[] o ) { return o ; }
+static private object[] _accept_START__end( object[] o ) { return o ; }
+
 
 /*
 atomic struct jo : <T,t> [, [remote]]    //_if(T==$undefined) T=$anonymous,t=$global
