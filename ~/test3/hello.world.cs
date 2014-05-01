dc89752d2e0bb3f878d41b8f7c8811d074114a32
Dzonatas <dzonatas@gmail.com>
2014-05-01 07:56:12 -0700

    Made test case 3.

---
+++ ~/test3/hello.world.cs
@@ -0,0 +1,20 @@
+public class Test2
+	{
+	static string[] text = { "Hello", "World" } ;
+	static void Main()
+		{
+		int i = 0 ;
+		string a, b = "" ;
+		label:
+		switch( i )
+			{
+			case 0  : a = text[0] ; break ;
+			case 2  : a = text[2] ; break ;
+			default : a = " "     ; break ;
+			}
+		b += a ;
+		if( ++i < 3 )
+			goto label ;
+		System.Console.WriteLine( b ) ;
+		}
+	}
