4c83a2db9509743873799c0e921162da7a55261d
Dzonatas <dzonatas@gmail.com>
2014-05-11 16:24:09 -0700

    Updated test cases for directory structure.

--- ~/test.sh
+++ ~/test.sh
@@ -5,7 +5,7 @@ ID="5a7160ed-13d5-4923-a1f9-3e32a47d558a"
 
 cd ./bin/Debug \
   && ./ecma.exe --shell="sh ../../~/compile.sh ../../~/hello.mono.world.il.text" \
-  && gcc -std=c99 -O3 -S -I ../../# /tmp/$ID.program.c -o /tmp/$ID.native.assembly.s \
-  && gcc -std=c99 -S -I ../../# /tmp/$ID.program.c -o /tmp/$ID.unoptimized.s \
-  && gcc -std=c99 -O3 /tmp/$ID.native.assembly.s -o /tmp/$ID.hello.world.exe \
+  && gcc -std=c99 -O3 -S -I ../../# /tmp/.$ID.d/program.c -o /tmp/.$ID.d/native.assembly.s \
+  && gcc -std=c99 -S -I ../../# /tmp/.$ID.d/program.c -o /tmp/.$ID.d/unoptimized.s \
+  && gcc -std=c99 -O3 /tmp/.$ID.d/native.assembly.s -o /tmp/$ID.hello.world.exe \
   && time /tmp/$ID.hello.world.exe
--- ~/test3/test.sh
+++ ~/test3/test.sh
@@ -3,12 +3,12 @@
 # Example: sh ./~/test3/test.sh
 ID="5a7160ed-13d5-4923-a1f9-3e32a47d558a"
 
-gmcs ./~/test3/hello.world.cs -out:/tmp/$ID.hello.world.exe
-monodis /tmp/$ID.hello.world.exe --output=/tmp/$ID.il.text
+gmcs ./~/test3/hello.world.cs -out:/tmp/.$ID.d/hello.world.exe
+monodis /tmp/.$ID.d/hello.world.exe --output=/tmp/.$ID.d/il.text
 
 cd ./bin/Debug \
-  && ./ecma.exe --shell="sh ../../~/compile.sh /tmp/$ID.il.text" \
-  && gcc -std=c99 -O3 -S -I ../../# /tmp/$ID.program.c -o /tmp/$ID.native.assembly.s \
-  && gcc -std=c99 -S -I ../../# /tmp/$ID.program.c -o /tmp/$ID.unoptimized.s \
-  && gcc -std=c99 -O3 /tmp/$ID.native.assembly.s -o /tmp/$ID.hello.world.exe \
+  && ./ecma.exe --shell="sh ../../~/compile.sh /tmp/.$ID.d/il.text" \
+  && gcc -std=c99 -O3 -S -I ../../# /tmp/.$ID.d/program.c -o /tmp/.$ID.d/native.assembly.s \
+  && gcc -std=c99 -S -I ../../# /tmp/.$ID.d/program.c -o /tmp/.$ID.d/unoptimized.s \
+  && gcc -std=c99 -O3 /tmp/.$ID.d/native.assembly.s -o /tmp/$ID.hello.world.exe \
   && time /tmp/$ID.hello.world.exe
