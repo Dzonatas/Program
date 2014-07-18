d4a6be8510189291496a32d1ed3d49b5a04d3e28
Dzonatas <dzonatas@gmail.com>
2014-07-18 14:59:38 -0700

    Commented: Switched.

--- #/Board.cs
+++ #/Board.cs
@@ -3,4 +3,7 @@ static class Board
 class Post   : /* [Snew] */ global::Post   {}
 class Put    : /* [Snew] */ global::Put    {}
 class Delete : /* [Snew] */ global::Delete {}
+//class "Signal"
+//	: Automaton
+//...
 }
