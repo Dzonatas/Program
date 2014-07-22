e80c5d5eec66227fabd55e2aaed922a21d823266
Dzonatas <dzonatas@gmail.com>
2014-07-22 12:55:07 -0700

    Zoned for programs.

--- #/X.CreateSimpleWindow.cs
+++ #/X.CreateSimpleWindow.cs
@@ -63,16 +63,25 @@ partial class X
 		switch( _event.Type )
 			{
 			case 4:
+				if( zone.Type != _event.Type )
+					{
+					XStart.ip = zone ;
+					return ;
+					}
+				break ;
 			case 5:
 				if( zone.Type != _event.Type )
+					{
+					XStop.time = zone ;
 					return ;
+					}
 				break ;
 			default:
 				zone = _event.XAny ;
-				System.Console.WriteLine( "Event type: {0} {1}", _event.Type, _event.XAny.Serial ) ;
 				Simple.Redraw() ;
 				goto loop ;
 			}
+		System.Console.WriteLine( "zone: {0} {1}", _event.Type, _event.XAny.Serial ) ;
 		for(;;) goto loop ;
 		}
 	class XStart : Zone.Start
