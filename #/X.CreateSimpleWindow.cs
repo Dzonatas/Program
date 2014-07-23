120dc5e6ef3b13c5e9fad2ffeb4f834d77ff51d4
Dzonatas <dzonatas@gmail.com>
2014-07-23 10:00:29 -0700

    Implemented QR splash screen.

--- #/X.CreateSimpleWindow.cs
+++ #/X.CreateSimpleWindow.cs
@@ -12,8 +12,10 @@ partial class X
 	static public XEvent _event ;
 	public static class Simple
 		{
+		static Random bit = new Random() ;
 		static public IntPtr drawable ;
 		static public IntPtr gc ;
+		static public IntPtr gc_erase ;
 		static public Values values ;
 		static public long   mask = GCValue.Function | GCValue.Background | GCValue.Foreground ;
 		static Simple()
@@ -24,6 +26,8 @@ partial class X
 			#if DEBUG
 			System.Console.WriteLine( "X-ServerVendor: {0}", x_server_vendor ) ;
 			#endif
+			values.Function  = GX.Clear ;
+			ʄ.CreateGC( drawable, mask, ref values, out gc_erase ) ;
 			ʄ.GCValues( ʄ.DefaultGC(), mask, out values ) ;
 			values.Function  = GX.Set ;
 			ʄ.CreateGC( drawable, mask, ref values, out gc ) ;
@@ -33,11 +37,29 @@ partial class X
 			{
 			ʄ.MapWindow( drawable ) ;
 			}
-		static public void Redraw()
+		static public void plot( int x, int y, int size )
 			{
-			Random y = new Random() ;
+			IntPtr _gc ;
+			if( bit.Next(2) == 0 ? false : true )
+				_gc = gc ;
+			else
+				_gc = gc_erase ;
+			for( int x_i = 0 ; x_i < size ; x_i++ )
+				for( int y_i = 0 ; y_i < size ; y_i++ )
+					if( x+x_i < 300 && y+y_i < 300 )
+						ʄ.DrawPoint( drawable, _gc, x+x_i, y+y_i ) ;
+			}
+		static public void QuickResponseEncodedSplash()
+			{
+			ʄ.DrawPoint( drawable, gc, 0, 0 ) ;
+			ʄ.DrawPoint( drawable, gc_erase, 0, 0 ) ;
+			for( int i = bit.Next(4) ; i > 0 ; i-- )
+				for( int x = 0 ; x < 300 ; x+= i+1 )
+					for( int y = 0 ; y < 300 ; y+= i+1 )
+						plot( x, y, i+1 ) ;
+			Random yy = new Random() ;
 			for( int x = 0 ; x < 300 ; x++ )
-				ʄ.DrawPoint( drawable, gc, x, y.Next( 300 ) ) ;
+					plot( x,  yy.Next(300), 1 ) ;
 			}
 		}
 	static XAnyEvent zone ;
@@ -64,7 +86,7 @@ partial class X
 				break ;
 			default:
 				zone = _event.XAny ;
-				Simple.Redraw() ;
+				Simple.QuickResponseEncodedSplash() ;
 				goto loop ;
 			}
 		System.Console.WriteLine( "zone: {0} {1}", _event.Type, _event.XAny.Serial ) ;
