0fcd5ab9ef00fb677be664f8a0a060bd3f8f8c06
Dzonatas <dzonatas@gmail.com>
2014-09-15 17:57:28 -0700

    Used physical device for pivotal data requirement, (spring-book).

---
+++ #/System.Extensions.Fan.cs
@@ -0,0 +1,21 @@
+namespace System.Extensions {
+using System.Text.RegularExpressions ;
+using System.Runtime.InteropServices ;
+using System.Drawing ;
+using X.Predefined ;
+using Drawable  = System.IntPtr ;         //_window,_pixmap
+using Rectangle = System.IntPtr ;         //X-defined-default:[x,y]:=upper-left
+public static class Solution
+	{
+	public static int Fan(this double _)
+		{
+		//'Post /etc/wall/fire.d nice/0.0'
+		return 0 ;
+		}
+	public static int FAN(this double _)
+		{
+		//'Post /etc/heading.d nice/0.0'
+		return 0 ;
+		}
+	}
+}
