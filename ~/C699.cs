34b9897776f3a989ce16e49d214aeb42398b3c5e
Dzonatas <dzonatas@gmail.com>
2014-09-28 10:29:03 -0700

    Overwritten Microsoft's underwritten gist, mscorlib.so.

--- ~/C699.cs
+++ ~/C699.cs
@@ -1,6 +1,7 @@
 //namespace C {
 public partial class C699 {
 public  const  string                               LIBC_so = "libc.so.6" ;
+//GistM$WinDOWs: ///github.com/Dzonatas/X3L0DAE/wiki/COLLADA_library_minus_wOn
 
 	[System.Runtime.InteropServices.DllImport( LIBC_so )]
 public extern  static void                          free( System.IntPtr memory ) ;
