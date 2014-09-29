773a0c953f9ebce8a50b14bc358218e20752db2f
Dzonatas <dzonatas@gmail.com>
2014-09-28 19:48:42 -0700

    (COLAB,AI) Resolving atonic affinity.

--- #/Program.cs
+++ #/Program.cs
@@ -289,17 +289,7 @@ partial class Program
 	static public void WriteC_Main()
 		{
 		var sw = C699.Main.C() ;
-		var c = C_Function.FromSymbol( "main" ) ;
-		c.Args = "( int argc , char** args , char** env )" ;
-		var e = A335.Method.EntryPoint.Head ;
-		if( A335.Method.cctorset.Contains(e.ClassType) )
-			{
-			c.Statement( "extern void " + e.ClassType + "_cctor()" ) ;
-			c.Statement( e.ClassType + "_cctor()" ) ;
-			}
-		c.Statement( "extern void " + e.ClassType + "$Main()" ) ;
-		c.Statement( e.ClassType + "$Main()" ) ;
-		c.WriteTo( sw ) ;
+		C699_Main_Function().WriteTo( sw ) ;
 		C_TypeDef.WriteTo( sw ) ;
 		foreach( C_Function f in c_functionset.Values )
 			{
--- ~/C699.Main.cs
+++ ~/C699.Main.cs
@@ -18,3 +18,24 @@ public partial class C699 {
 			}
 		}
 }//}
+
+public partial class A335
+{
+partial class Program
+	{
+	public static C_Function C699_Main_Function()
+		{
+		var c = C_Function.FromSymbol( "main" ) ;
+		c.Args = "( int argc , char** args , char** env )" ;
+		var e = A335.Method.EntryPoint.Head ;
+		if( A335.Method.cctorset.Contains(e.ClassType) )
+			{
+			c.Statement( "extern void " + e.ClassType + "_cctor()" ) ;
+			c.Statement( e.ClassType + "_cctor()" ) ;
+			}
+		c.Statement( "extern void " + e.ClassType + "$Main()" ) ;
+		c.Statement( e.ClassType + "$Main()" ) ;
+		return c ;
+		}
+	}
+}
