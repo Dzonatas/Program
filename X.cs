459dc6d5bc07a88d3e7a6ceb364553c47c50abbe
Dzonatas <dzonatas@gmail.com>
2015-05-07 20:22:12 -0700

    Refactored <A335-Xo_t-_io-_1>

--- X.cs
+++ X.cs
@@ -334,12 +334,22 @@ class Xo_t
 		X.Auto["list"] = list ;
 		if( i == 0 || i >= xo_t.Length )
 			return (io_volatile ? "" : put("A335-Xo_t-_io-1")) ;
+		string _list = tab ;
+		_list += "static int __" + i.ToString() + "()"+tab ;
+		_list += "{" + tab ;
+		_list += "backup = " + xo_t[i].rhs.Length.ToString() + " ;" + tab ;
+		_list += "auto = new "
+			+ xo_t[i].lhs.s + "._" + xo_t[i].lhs.X
+			+ "." + X.Auto[ "_"+xo_t[i].lhs.X ] + "() ;" + tab ;
+		_list += "yy = " + ((int)xo_t[i]).ToString() + " ;" + tab ;
+		_list += "return -" + i.ToString() + " ;" + tab ;
+		_list += "}" + tab ;
 		X.Auto["namespace"] = xo_t[i].lhs.s + "._" + xo_t[i].lhs.X ;
 		X.Auto["signal"]    = X.Auto[ "_"+xo_t[i].lhs.X ] ;
 		X.Auto["argc"]      = xo_t[i].rhs.Length.ToString() ;
 		X.Auto["i"]         = '-'+i.ToString() ;
 		X.Auto["I"]         = ((int)xo_t[i]).ToString() ;
-		return put("A335-Xo_t-_io-_1")+(io_volatile ? "" : put("A335-Xo_t-_io-1")) ;
+		return _list+(io_volatile ? "" : put("A335-Xo_t-_io-1")) ;
 		}
 	static bool return_rule( int i, string rule, ref string list, string _a )
 		{
