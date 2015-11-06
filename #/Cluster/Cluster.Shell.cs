9ff33b80f5aed078ed094e8c89f96283a38eb6aa
Dzonatas <dzonatas@gmail.com>
2015-11-06 14:03:50 -0800

    Moved Cluster namespace to new folder.

--- #/Cluster/Cluster.Shell.cs
+++ #/Cluster/Cluster.Shell.cs
@@ -1,6 +1,81 @@
-namespace Cluster {
+namespace Cluster
+{
 public static partial class Shell
 	{
+	[System.Runtime.InteropServices.DllImport( "libc.so.6" )]	public extern  static   int             system(string command) ;
+	static System.Diagnostics.ProcessStartInfo psi ;
+	static public string Dpkg__p_LSB()
+		{
+		psi = new System.Diagnostics.ProcessStartInfo( "/usr/bin/env", "dpkg -p LSB" ) ;
+		psi.UseShellExecute          = false ;
+		psi.StandardOutputEncoding   = System.Text.Encoding.ASCII ;
+		psi.RedirectStandardOutput   = true ;
+		psi.RedirectStandardInput    = true ;
+		psi.CreateNoWindow           = true ;
+		//var f = System.IO.File.OpenText( hello ) ;
+		System.Text.StringBuilder sb = new System.Text.StringBuilder() ;
+		System.Diagnostics.Process p ;
+		//try {
+			p= System.Diagnostics.Process.Start(psi) ;
+			//string s = f.ReadToEnd() ;
+			p.StandardInput.AutoFlush = true ;
+			//p.StandardInput.Write( s ) ;
+			p.WaitForInputIdle() ;
+			p.StandardInput.Close() ;
+			while(!p.StandardOutput.EndOfStream)
+				{
+				string x = p.StandardOutput.ReadLine() ;
+				if( x == null )
+					break ;
+				sb.Append( x ) ;
+				sb.Append( '\n' ) ;
+				}
+			goto done ;
+			//} catch {}
+		done:
+		p.WaitForExit() ;
+		return sb.ToString() ;
+		}
+	static public string Dpkg__p_START()
+		{
+		psi = new System.Diagnostics.ProcessStartInfo( "/usr/bin/env", "sensors" ) ;
+		psi.UseShellExecute          = false ;
+		psi.StandardOutputEncoding   = System.Text.Encoding.ASCII ;
+		psi.RedirectStandardOutput   = true ;
+		psi.RedirectStandardInput    = true ;
+		psi.CreateNoWindow           = true ;
+		//var f = System.IO.File.OpenText( hello ) ;
+		System.Text.StringBuilder sb = new System.Text.StringBuilder() ;
+		System.Diagnostics.Process p ;
+		//try {
+			p= System.Diagnostics.Process.Start(psi) ;
+			//string s = f.ReadToEnd() ;
+			p.StandardInput.AutoFlush = true ;
+			//p.StandardInput.Write( s ) ;
+			p.WaitForInputIdle() ;
+			p.StandardInput.Close() ;
+			while(!p.StandardOutput.EndOfStream)
+				{
+				string x = p.StandardOutput.ReadLine() ;
+				if( x == null )
+					break ;
+				sb.Append( x ) ;
+				sb.Append( '\n' ) ;
+				}
+			goto done ;
+			//} catch {}
+		done:
+		p.WaitForExit() ;
+		return sb.ToString() ;
+		}
+	}
+}
+
+namespace Cluster
+{
+public static partial class Shell
+	{
+	readonly static char[] MSB = new char[] { ' ', '.' } ;
 	static string[] _sys_power_state     = { "", "", "", "", "", "", "", "", "" } ;
 	public static int Cat__sys_power_state()
 		{
@@ -32,7 +107,7 @@ public static partial class Shell
 			//} catch {}
 		done:
 		p.WaitForExit() ;
-		return ( _sys_power_state = sb.ToString().Split( A335.MSB ) ).Length ;
+		return ( _sys_power_state = sb.ToString().Split( MSB ) ).Length ;
 		}
 	public static int Test__sys_class___sys_power_state_() //Test__sys_class.(_sys_power_state)
 		{
@@ -64,7 +139,7 @@ public static partial class Shell
 			//} catch {}
 		done:
 		p.WaitForExit() ;
-		return sb.ToString().Split( A335.MSB ).Length ;
+		return sb.ToString().Split( MSB ).Length ;
 		}
 	static public int Sensors___grep_ALARM()
 		{
---
+++ #/Cluster/Cluster.cs
@@ -0,0 +1,53 @@
+namespace Cluster {
+//	[0.0::window,geometry:full.screen:0]
+class system {
+static system() {
+ ///www.debian.org/News/2014/20140908
+ /* X.Y.MapZ() */
+}
+public class Post {} ;
+public class Put {} ;
+public class Get {} ;
+public class Delete {} ;
+} ;
+public static class Inbox {} ;  //(CTS)->MIME_t
+public static class Outbox {} ; //MIME_t->(CTS)
+public class Social
+	{
+	public class Security
+		{
+		static System.IO.MemoryStream license ;
+		static Security() { license = null ; }
+		public struct register
+			{
+			public int[]     _SSNo ;
+			public string[]  _NAMe ;
+			public A335.Zone _DISp ;
+			//system.ARM[]   __PCi ;
+			}
+		}
+	} ; //MIME_t->(MIME)
+public interface Screen
+	{
+	Idol      Saver        { set ; get ; }
+	Idle      Word         { set ; get ; }
+	double    BackAngle    { set ; get ; }
+	bool      Standard     { set ; get ; }
+	bool      UnifiedPort  { set ; get ; } //IPv6-UDP-Content-Length:[0|#]
+	Passport  Present      { set ; }
+	}
+public class Passport
+	{
+	System.DateTime[] dt ;
+	System.DateTime[] prefixed ;
+	System.DateTime[] suffixed ;
+	System.DateTime[] objected ;
+	}
+public class Idle {} ;
+public class Idol {} ;
+public class Logical {} ;
+public class Presence {} ;
+}
+
+
+
