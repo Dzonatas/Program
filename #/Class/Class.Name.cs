36d57f1458ca0241255897585351730b8f401c25
Dzonatas <dzonatas@gmail.com>
2015-11-18 12:28:27 -0800

    Implemented Class.Name.fullName.

--- #/Class/Class.Name.cs
+++ #/Class/Class.Name.cs
@@ -2,12 +2,26 @@ partial class A335
 {
 public partial class Class : Automatrix
 	{
-	public partial class Name : Class {}
+	public partial class Name : Class
+		{
+		static public implicit operator string( Name n ) { return n.fullName ; }
+		protected virtual string fullName { get { throw new System.NotImplementedException() ; } }
+		}
 	}
 
 public partial class   className_____name1_____slashedName
-	: Class.Name {}
+	: Class.Name {
+	protected override string fullName
+		{
+		get { return "["+Arg2.Token+"]"+string.Join( ".", Arg4.ResolveType() ) ; }
+		}
+	}
 
 public partial class   className_slashedName
-	: Class.Name {}
+	: Class.Name {
+	protected override string fullName
+		{
+		get { return ""+string.Join( ".", Arg1.ResolveType() ) ; }
+		}
+	}
 }
