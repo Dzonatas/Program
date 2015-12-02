f05c1e172154dd53643d3c0cc43d9473cb8b5481
Dzonatas <dzonatas@gmail.com>
2015-12-02 10:20:12 -0800

    Implemented ldc.i4.

---
+++ #/Instr/Instr.I.cs
@@ -0,0 +1,22 @@
+partial class A335
+{
+partial class Instr : Automatrix
+	{
+	public partial class I : Instr {}
+	}
+
+public partial class   instr_INSTR_I_int32
+	: Instr.I       {
+	protected override void render()
+		{
+		switch( Op )
+			{
+			case "LDC_I4" :
+				oprand.C.Push( new C699.c( Arg2.Token ), C_Type.Acquire( C699.C.Int ) ) ;
+				break ;
+			default :
+				throw new System.NotImplementedException( Op ) ;
+			}
+		}
+	}
+}
