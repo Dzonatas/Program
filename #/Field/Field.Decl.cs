e6a66c4a8f699e32d4174bfb53005bd83d84bf21
Dzonatas <dzonatas@gmail.com>
2015-12-29 08:09:03 -0800

    Removed Field.Decl.field.

--- #/Field/Field.Decl.cs
+++ #/Field/Field.Decl.cs
@@ -4,11 +4,6 @@ public partial class Field
 	{
 	public partial class Decl : Automatrix
 		{
-		protected string field ;
-		public static implicit operator string( Decl decl )
-			{
-			return decl.field ;
-			}
 		public virtual string ToStructField( Class.Head h )
 			{
 			throw new System.NotImplementedException() ;
@@ -20,13 +15,13 @@ public partial class   fieldDecl___field__repeatOpt_fieldAttr_type_id_atOpt_init
 	: Field.Decl	{
 	public override string ToStructField( Class.Head h )
 			{
-			string field = string.Empty ;
-			A335.Type type = Argv[4] as A335.Type ;
-			string t = Arg5.Token.Replace('<', '_').Replace('>', '_') ;
+			string     field ;
+			A335.Type  type  = Argv[4] as A335.Type ;
+			string     id    = Arg5.Token.Replace('<', '_').Replace('>', '_') ;
 			if( h.ValueType )
-				field += C699.C.Struct( type, t ) ;
+				field = C699.C.Struct( type, id ) ;
 			else
-				field += C699.C.Struct( type, h.Symbol + "$" + t ) ;
+				field = C699.C.Struct( type, h.Symbol + "$" + id ) ;
 			field += " ;" ;
 			return field ;
 			}
