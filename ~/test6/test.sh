1cb2343d9008e76d3fa4c1d10613920ba5ca0e01
Dzonatas <dzonatas@gmail.com>
2015-12-30 15:51:14 -0800

    Added Test6 to ecma.csproj.

---
+++ ~/test6/test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Run test from top directory of repository
+# Example: sh ./~/test6/test.sh
+ID="5a7160ed-13d5-4923-a1f9-3e32a47d558a"
+export TERM="XHTML"
+
+[ ! -d "/tmp/.$ID.d" ] && mkdir /tmp/.$ID.d
+gmcs ./~/test6/hello.world.6.cs -out:/tmp/.$ID.d/dis.test6.hello.world.exe
+monodis /tmp/.$ID.d/dis.test6.hello.world.exe --output=/tmp/.$ID.d/test6.il.text
+
+exec ./~/_test.sh /tmp/.$ID.d/test6.il.text test6
